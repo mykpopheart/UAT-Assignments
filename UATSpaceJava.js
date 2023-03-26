0a4b89ccf34c8adf527603753d3f99ffc0ec3d39
mykpopheart <125770800+mykpopheart@users.noreply.github.com>
2023-03-25 18:32:19 -0700

    Add files via upload

--- UATSpaceJava.js
+++ UATSpaceJava.js
@@ -17,4 +17,15 @@ function startCountdown() {
       countdownElement.innerText = "Blastoff";
     }
   }, 1000);
+  <audio controls>
+    <source src="us-lab-background.mp3" type="audio/mpeg"></source>  
+</audio>
 }
+<script>
+		var nameInput = document.getElementById("nameInput");
+		var submitButton = document.getElementById("submitButton");
+
+		submitButton.addEventListener("click", function() {
+			alert("Hello, " + nameInput.value + "!")
+		});
+	</script>
